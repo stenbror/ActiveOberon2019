7fea34cda3219c8118a7c2bb5d2bb4bbba5932f8
Richard Magnor Stenbro <stenbror@hotmail.com>
2024-08-01 18:12:11 +0200

    Added header text

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 mod scanner;
 mod parser;
 
+use inline_colorization::*;
+
 use crate::scanner::{Scanner, ScannerMethods, Symbols};
 use crate::parser::{Parser, ParseMethods, SyntaxNode};
 
+const COMPILE_DATE_STRING: &str = compile_time::date_str!();
+
 fn main() {
+
+    let version = option_env!("PROJECT_VERSION").unwrap_or(env!("CARGO_PKG_VERSION"));
 
     let mut parser = Parser::new("code test end ", false);
     parser.advance();
+    let symb = parser.parse_expression();
 
-    println!("Hello, world!");
+    println!("\r\n{color_bright_green}Active Oberon (2019 Revision){color_reset} Compiler, Version {version} [Build: {COMPILE_DATE_STRING}]\r\n");
 }
